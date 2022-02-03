27884b408466bbfe5e86d44900ebd1b38644d40e
Ye Yuan <khrylx@gmail.com>
2022-02-02 21:38:12 -0500

    minor fix

--- data/preprocessor_sdd.py
+++ data/preprocessor_sdd.py
@@ -1,5 +1,3 @@
-import pickle
-
 import torch, os, numpy as np, copy
 import cv2
 import glob
@@ -7,13 +5,8 @@ from .map import GeometricMap
 
 
 class SDDPreprocess(object):
-    def __init__(self,
-                 data_root,
-                 seq_name,
-                 parser,
-                 log,
-                 split='train',
-                 phase='training'):
+
+    def __init__(self, data_root, seq_name, parser, log, split='train', phase='training'):
         self.parser = parser
         self.dataset = parser.dataset
         self.data_root = data_root
@@ -21,8 +14,7 @@ class SDDPreprocess(object):
         self.future_frames = parser.future_frames
         self.frame_skip = parser.get('frame_skip', 1)
         self.min_past_frames = parser.get('min_past_frames', self.past_frames)
-        self.min_future_frames = parser.get('min_future_frames',
-                                            self.future_frames)
+        self.min_future_frames = parser.get('min_future_frames', self.future_frames)
         self.traj_scale = parser.traj_scale
         self.past_traj_scale = parser.traj_scale
         self.load_map = parser.get('load_map', False)
@@ -34,17 +26,6 @@ class SDDPreprocess(object):
 
         label_path = f'{data_root}/{split}/{seq_name}.txt'
         delimiter = ' '
-
-        self.gt = np.genfromtxt(label_path, delimiter=delimiter, dtype=str)
-        frames = self.gt[:, 0].astype(np.float32).astype(np.int)
-        fr_start, fr_end = frames.min(), frames.max()
-        self.init_frame = fr_start
-        self.num_fr = fr_end + 1 - fr_start
-
-        if self.load_map:
-            self.load_scene_map()
-        else:
-            self.geom_scene_map = None
 
         self.gt = np.genfromtxt(label_path, delimiter=delimiter, dtype=str)
         frames = self.gt[:, 0].astype(np.float32).astype(np.int)
@@ -89,12 +70,8 @@ class SDDPreprocess(object):
         cur_id = self.GetID(pre_data[0])
         valid_id = []
         for idx in cur_id:
-            exist_pre = [(False if isinstance(data, list) else
-                          (idx in data[:, 1]))
-                         for data in pre_data[:self.min_past_frames]]
-            exist_fut = [(False if isinstance(data, list) else
-                          (idx in data[:, 1]))
-                         for data in fut_data[:self.min_future_frames]]
+            exist_pre = [(False if isinstance(data, list) else (idx in data[:, 1])) for data in pre_data[:self.min_past_frames]]
+            exist_fut = [(False if isinstance(data, list) else (idx in data[:, 1])) for data in fut_data[:self.min_future_frames]]
             if np.all(exist_pre) and np.all(exist_fut):
                 valid_id.append(idx)
         return valid_id
@@ -116,18 +93,13 @@ class SDDPreprocess(object):
         map_vis_file = f'{self.data_root}/map_{self.map_version}/vis_{self.seq_name}.png'
         map_meta_file = f'{self.data_root}/map_{self.map_version}/meta_{self.seq_name}.txt'
         self.scene_map = np.transpose(cv2.imread(map_file), (2, 0, 1))
-        self.scene_vis_map = np.transpose(
-            cv2.cvtColor(cv2.imread(map_vis_file), cv2.COLOR_BGR2RGB),
-            (2, 0, 1))
+        self.scene_vis_map = np.transpose(cv2.cvtColor(cv2.imread(map_vis_file), cv2.COLOR_BGR2RGB), (2, 0, 1))
         self.meta = np.loadtxt(map_meta_file)
         self.map_origin = self.meta[:2]
         self.map_scale = scale = self.meta[2]
-        homography = np.array([[scale, 0., 0.], [0., scale, 0.],
-                               [0., 0., scale]])
-        self.geom_scene_map = GeometricMap(self.scene_map, homography,
-                                           self.map_origin)
-        self.scene_vis_map = GeometricMap(self.scene_vis_map, homography,
-                                          self.map_origin)
+        homography = np.array([[scale, 0., 0.], [0., scale, 0.], [0., 0., scale]])
+        self.geom_scene_map = GeometricMap(self.scene_map, homography, self.map_origin)
+        self.scene_vis_map = GeometricMap(self.scene_vis_map, homography, self.map_origin)
 
     def PreMotion(self, DataTuple, valid_id):
         motion = []
@@ -136,19 +108,13 @@ class SDDPreprocess(object):
             mask_i = torch.zeros(self.past_frames)
             box_3d = torch.zeros([self.past_frames, 2])
             for j in range(self.past_frames):
-                past_data = DataTuple[j]  # past_data
+                past_data = DataTuple[j]              # past_data
                 if len(past_data) > 0 and identity in past_data[:, 1]:
-                    found_data = past_data[past_data[:, 1] ==
-                                           identity].squeeze()[[
-                                               self.xind, self.zind
-                                           ]] / self.past_traj_scale
-                    box_3d[self.past_frames - 1 -
-                           j, :] = torch.from_numpy(found_data).float()
-                    mask_i[self.past_frames - 1 - j] = 1.0
+                    found_data = past_data[past_data[:, 1] == identity].squeeze()[[self.xind, self.zind]] / self.past_traj_scale
+                    box_3d[self.past_frames-1 - j, :] = torch.from_numpy(found_data).float()
+                    mask_i[self.past_frames-1 - j] = 1.0
                 elif j > 0:
-                    box_3d[self.past_frames - 1 -
-                           j, :] = box_3d[self.past_frames -
-                                          j, :]  # if none, copy from previous
+                    box_3d[self.past_frames-1 - j, :] = box_3d[self.past_frames - j, :]    # if none, copy from previous
                 else:
                     raise ValueError('current id missing in the first frame!')
             motion.append(box_3d)
@@ -162,15 +128,13 @@ class SDDPreprocess(object):
             mask_i = torch.zeros(self.future_frames)
             pos_3d = torch.zeros([self.future_frames, 2])
             for j in range(self.future_frames):
-                fut_data = DataTuple[j]  # cur_data
+                fut_data = DataTuple[j]              # cur_data
                 if len(fut_data) > 0 and identity in fut_data[:, 1]:
-                    found_data = fut_data[fut_data[:, 1] == identity].squeeze(
-                    )[[self.xind, self.zind]] / self.traj_scale
+                    found_data = fut_data[fut_data[:, 1] == identity].squeeze()[[self.xind, self.zind]] / self.traj_scale
                     pos_3d[j, :] = torch.from_numpy(found_data).float()
                     mask_i[j] = 1.0
                 elif j > 0:
-                    pos_3d[j, :] = pos_3d[j -
-                                          1, :]  # if none, copy from previous
+                    pos_3d[j, :] = pos_3d[j - 1, :]    # if none, copy from previous
                 else:
                     raise ValueError('current id missing in the first frame!')
             motion.append(pos_3d)
@@ -179,14 +143,13 @@ class SDDPreprocess(object):
 
     def __call__(self, frame):
 
-        assert frame - self.init_frame >= 0 and frame - self.init_frame <= self.TotalFrame(
-        ) - 1, 'frame is %d, total is %d' % (frame, self.TotalFrame())
+        assert frame - self.init_frame >= 0 and frame - self.init_frame <= self.TotalFrame() - 1, 'frame is %d, total is %d' % (frame, self.TotalFrame())
+
 
         pre_data = self.PreData(frame)
         fut_data = self.FutureData(frame)
         valid_id = self.get_valid_id(pre_data, fut_data)
-        if len(pre_data[0]) == 0 or len(
-                fut_data[0]) == 0 or len(valid_id) == 0:
+        if len(pre_data[0]) == 0 or len(fut_data[0]) == 0 or len(valid_id) == 0:
             return None
 
         if self.dataset == 'nuscenes_pred':
